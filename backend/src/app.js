e6b0c650ac5cb6d80187be180a4460c6973ec304
Abhishek-Sahu532 <asahu532@gmail.com>
2024-05-26 14:39:45 +0530

    Trying to build

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,15 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 const app = express();
+import path from 'path'
+
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 app.use(
   cors({
