c2ed8901d725c31af29718bda43e18a43d67ad44
Abhishek Sahu <asahu532@gmail.com>
2024-05-27 18:08:31 +0530

    Update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -6,7 +6,9 @@ const connectDB = async () =>{
     try {
         let DbInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`Database Connection: ${DbInstance.connection.host}`)
+  console.log('from the try block',   process.env.MONGODB_URI)
     } catch (error) {
+         console.log('from the catch block',   process.env.MONGODB_URI)
         console.log('MongoDB conntection failed', error);
         process.exit(1)
     }
