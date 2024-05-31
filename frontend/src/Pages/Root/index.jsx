bf8ae5e0ffed4e8b0d67680c238b6f54604480da
Abhishek-Sahu532 <asahu532@gmail.com>
2024-05-31 23:37:37 +0530

    fixing bugs

--- frontend/src/Pages/Root/index.jsx
+++ frontend/src/Pages/Root/index.jsx
@@ -1,46 +1,52 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Advertisement } from "../../Components/Events";
 import { VideoDetailsCard } from "../../Components/VideoDetailsCard";
 import { fetchAllVideos } from "../../actions/VideoAction";
 import { Loader } from "../../Components/Loader";
-
 import InfiniteScroll from "react-infinite-scroll-component";
-
 
 const Root = () => {
   const { loading, videos } = useSelector((state) => state.videos);
   const dispatch = useDispatch();
   const [page, setPage] = useState(1);
+  const [videoList, setVideoList] = useState(videos);
   const [hasMore, setHasMore] = useState(true);
+
   useEffect(() => {
     dispatch(fetchAllVideos(page));
-  }, [dispatch]);
+  }, [dispatch, page]);
+
 
   const fetchMoreData = () => {
- 
     setPage((prevPage) => prevPage + 1);
-    dispatch(fetchAllVideos(page));
-    console.log(page)
+    setVideoList((prevVideos) => [...prevVideos, ...videos]);
   };
+
+  useEffect(() => {
+    if (!loading && videos.length === 0) {
+      setHasMore(false);
+    }
+  }, [loading, videos]);
 
   return (
     <div>
-   
       <Advertisement />
-      {loading ? (
+      {loading  ? (
         <Loader />
       ) : (
-        <InfiniteScroll  dataLength={3}
-      next={fetchMoreData}
-      hasMore={hasMore}
-      loader={<Loader />}>
-        <div className="flex gap-10 p-8 flex-wrap justify-around">
-          {videos &&
-            videos.map((video, index) => (
-              <VideoDetailsCard vid={video} key={index} />
-            ))}
-        </div>
+        <InfiniteScroll
+          dataLength={videoList.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<Loader />}
+        >
+          <div className="flex gap-10 p-8 flex-wrap justify-around">
+            {videoList &&
+              videoList.map((video, index) => (
+                <VideoDetailsCard vid={video} key={index} />
+              ))}
+          </div>
         </InfiniteScroll>
       )}
     </div>
